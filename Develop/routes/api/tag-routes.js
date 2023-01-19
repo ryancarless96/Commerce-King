a3a1f86af781399686efa9eac1ec896348a6455e
Ryan Carless <ryancarless96@gmail.com>
2023-01-19 11:11:09 -0500

    commit changes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,24 +1,51 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+try {
+  const tagData = await Tag.findAll();
+  res.status(200).json(tagData);
+} catch(err) {
+  res.status(500).json(err);
+}
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Tag, through: Product, as: 'tag_products' }]
+    });
+    if(!tagData) {
+      res.status(404).json({message: 'No tags found with this id!'});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(400).json(err);
+  }
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  // try {
+  //   const tagData = await Tag.update(
+  //     {
+  //       tag_name: req.body.
+  //     }
+  //   )
+  // }
 });
 
 router.delete('/:id', (req, res) => {
